773387d081461190160ccc0ec181db4a6e0c7687
Vladimir Grankov <Vladimirgrankov@yandex.ru>
2023-08-01 13:26:12 +0300

    Открывается и закрывается часть 2

--- js/gallery-modal.js
+++ js/gallery-modal.js
@@ -1,14 +1,11 @@
-import { openPictureModal } from './modal.js';
+import { openPictureModal, modal } from './modal.js';
 import { createElemet } from './utils.js';
 
-const bigPictureImg = document.querySelector('.big-picture__img img');
-const likeCount = document.querySelector('.likes-count');
-const commentCount = document.querySelector('.comments-count');
-const commentsDescription = document.querySelector('.social__caption');
-const loadButton = document.querySelector('.comments-loader');
+const showCount = document.querySelector('.social__comment-count');
+const moreButton = document.querySelector('.comments-loader');
 const commentsList = document.querySelector('.social__comments');
-const blockCount = document.querySelector('.social__comment-count');
 const COMMENTS_TO_SHOW = 5;
+let currentComments = [];
 
 
 const createComment = (avatar, message, name) => {
@@ -27,9 +24,9 @@ const createComment = (avatar, message, name) => {
   return listItem;
 };
 
-const renderComments = (commentsData) => {
+const renderComments = (data) => {
   const fragment = document.createDocumentFragment();
-  commentsData.forEach(({ avatar, message, name }) => {
+  data.forEach(({ avatar, message, name }) => {
     const commentElement = createComment(avatar, message, name);
     fragment.append(commentElement);
   });
@@ -37,35 +34,44 @@ const renderComments = (commentsData) => {
   return fragment;
 };
 
-const initFirstComments = (comments) => {
-  const showComments = comments.slice(0, COMMENTS_TO_SHOW);
-  const renderFirstComments = renderComments(showComments);
+const initPartComments = (comments) => {
+  const toShowComments = comments.slice(0, COMMENTS_TO_SHOW,);
+  const renderFirstComments = renderComments(toShowComments);
 
+  if (toShowComments.length === comments.length) {
+    moreButton.classList.add('hidden');
+  } else {
+    moreButton.classList.remove('hidden');
+  }
 
   commentsList.append(renderFirstComments);
-
-  loadButton.addEventListener('click', (items) => {
-    commentsList.append(...items).map(renderComments);
-  });
-  blockCount.firstChild.textContent = `${showComments.length} из `;
+  showCount.firstChild.textContent = `${toShowComments.length} из `;
 };
 
 const loadMore = () => {
+  const moreComments = currentComments.slice(commentsList.children.length, commentsList.children.length + COMMENTS_TO_SHOW);
+  const renderMoreComments = renderComments(moreComments);
+  commentsList.append(renderMoreComments);
 
+  if (currentComments.length === commentsList.children.length) {
+    moreButton.classList.add('hidden');
+  }
+
+  showCount.firstChild.textContent = `${commentsList.children.length} из `;
 };
 
-
-const createPictureModal = (postData) => {
-  const {url, likes, comments, description} = postData;
-  bigPictureImg.src = url;
-  likeCount.textContent = likes;
-  commentCount.textContent = comments.length;
-  commentsDescription.textContent = description;
+const createPictureModal = (data) => {
+  const {url, likes, comments, description} = data;
+  modal.querySelector('.big-picture__img img').src = url;
+  modal.querySelector('.likes-count').textContent = likes;
+  modal.querySelector('.comments-count').textContent = comments.length;
+  modal.querySelector('.social__caption').textContent = description;
 
   commentsList.innerHTML = '';
-  initFirstComments(comments);
-
-  openPictureModal(postData);
+  currentComments = comments;
+  moreButton.addEventListener('click', loadMore);
+  initPartComments (comments);
+  openPictureModal(data);
 };
 
-export { createPictureModal, renderComments };
+export { createPictureModal };
